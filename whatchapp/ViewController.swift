91ba7b2846fda16fd38eb51b9c23781ff47e3862
PhilippMatthes <github@philippmatth.es>
2018-07-22 09:40:45 +0200

    Initial Commit

---
+++ whatchapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whatchapp
+//
+//  Created by Philipp Matthes on 22.07.18.
+//  Copyright © 2018 Philipp Matthes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
